cf10c19e7b28204b713eb4156ebb3f42a886fc2b
Erlend Klakegg Bergheim <erlend@klakegg.net>
2015-12-08 09:04:19 +0100

    Adding TransportProtocol.

---
+++ peppol-common/src/main/java/no/difi/vefa/peppol/common/model/TransportProtocol.java
@@ -0,0 +1,47 @@
+package no.difi.vefa.peppol.common.model;
+
+import no.difi.vefa.peppol.common.lang.PeppolException;
+
+import java.util.regex.Pattern;
+
+public class TransportProtocol {
+
+    private static Pattern pattern = Pattern.compile("[\\p{Upper}\\d]+");
+
+    public static final TransportProtocol AS2 = new TransportProtocol("AS2");
+    public static final TransportProtocol AS4 = new TransportProtocol("AS4");
+    public static final TransportProtocol INTERNAL = new TransportProtocol("INTERNAL");
+
+    public static TransportProtocol forIdentifier(String identifier) throws PeppolException {
+        if (!pattern.matcher(identifier).matches())
+            throw new PeppolException("Identifier not according to pattern.");
+
+        return new TransportProtocol(identifier);
+    }
+
+    private String identifier;
+
+    private TransportProtocol(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TransportProtocol that = (TransportProtocol) o;
+
+        return !(identifier != null ? !identifier.equals(that.identifier) : that.identifier != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return identifier != null ? identifier.hashCode() : 0;
+    }
+}
---
+++ peppol-common/src/test/java/no/difi/vefa/peppol/common/model/TransportProtocolTest.java
@@ -0,0 +1,35 @@
+package no.difi.vefa.peppol.common.model;
+
+import no.difi.vefa.peppol.common.lang.PeppolException;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TransportProtocolTest {
+
+    @Test
+    public void simple() throws PeppolException {
+        Assert.assertTrue(TransportProtocol.AS2.equals(TransportProtocol.forIdentifier(TransportProtocol.AS2.getIdentifier())));
+
+        Assert.assertTrue(throwsException("As2"));
+        Assert.assertTrue(throwsException("as2"));
+        Assert.assertTrue(throwsException("as-2"));
+        Assert.assertTrue(throwsException("AS2 "));
+        Assert.assertTrue(throwsException("AS2-PEPPOL"));
+        Assert.assertTrue(throwsException("AS2_1"));
+
+        Assert.assertFalse(throwsException(TransportProtocol.AS2.getIdentifier()));
+        Assert.assertFalse(throwsException(TransportProtocol.AS4.getIdentifier()));
+        Assert.assertFalse(throwsException(TransportProtocol.INTERNAL.getIdentifier()));
+        Assert.assertFalse(throwsException("FUTURE"));
+    }
+
+    private boolean throwsException(String identifier) {
+        try {
+            TransportProtocol.forIdentifier(identifier);
+            return false;
+        } catch (PeppolException e) {
+            return true;
+        }
+    }
+
+}
