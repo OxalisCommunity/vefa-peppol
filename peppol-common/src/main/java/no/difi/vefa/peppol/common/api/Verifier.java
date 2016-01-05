c0475fd50b173c6cc480eb4185f8139b0b6678e7
Erlend Klakegg Bergheim <erlend@klakegg.net>
2016-01-05 17:23:52 +0100

    Removing classes.

--- peppol-common/src/main/java/no/difi/vefa/peppol/common/api/Verifier.java
+++
@@ -1,9 +0,0 @@
-package no.difi.vefa.peppol.common.api;
-
-import no.difi.vefa.peppol.common.lang.PeppolException;
-
-public interface Verifier<T> {
-
-    void verify(T o) throws PeppolException;
-
-}
