e665fdac50629b4a4bb4fa32daa1323129379ed8
Erlend Klakegg Bergheim <erlend@klakegg.net>
2017-01-21 11:44:09 +0100

    Updating certificate validator.

--- peppol-lookup/src/main/java/no/difi/vefa/peppol/lookup/fetcher/BasicApacheFetcher.java
+++ peppol-lookup/src/main/java/no/difi/vefa/peppol/lookup/fetcher/BasicApacheFetcher.java
@@ -31,7 +31,6 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 
 import java.io.ByteArrayInputStream;
 import java.net.SocketException;
